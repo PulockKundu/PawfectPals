97ca0a957811cdd0df88324048ac729921ad42ef
PulockKundu <pulock7643@gmail.com>
2026-01-16 02:08:40 +0600

    signup updated

--- Delivery_Staff/View/signup.php
+++ Delivery_Staff/View/signup.php
@@ -8,49 +8,67 @@ if($isLoggedIn){
 }
 $emailErr = $_SESSION["emailErr"] ?? "";
 $passwordErr = $_SESSION["passwordErr"] ?? "";
-$loginErr = $_SESSION["signUpErr"] ?? "";
+$usertypeErr = $_SESSION["usertypeErr"] ?? "";
+$signUpErr = $_SESSION["signUpErr"] ?? "";
 
 $previousValues = $_SESSION["previousValues"] ?? [];
-
+$usertype = $previousValues['usertype'] ?? '';
 
 unset($_SESSION["previousValues"]);
 unset($_SESSION["emailErr"]);
 unset($_SESSION["passwordErr"]);
+unset($_SESSION["usertypeErr"]);
 unset($_SESSION["signUpErr"]);
-unset($_SESSION["loginErr"]);
+
+
 
 
 ?>
 
 <html>
-
+<head>
+    <title>Signup</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
 <body>
 <!-- <pre><?php echo $previousValues["email"];?></php></pre> -->
+ <div class="login-container">
 <form method="post" onsubmit="" action="..\controller\signup_validation.php" enctype="multipart/form-data">
     <table>
         <tr>
             <td>Email</td>
             <td><input type="text" name="email"  placeholder="Enter your email" value="<?php echo $previousValues["email"] ?? "" ?>"/> </td>
-            <td><?php echo $emailErr; ?></td>
+            <td class="error"><?php echo $emailErr; ?></td>
         </tr>
         <tr>
             <td>Password</td>
             <td><input type="password" name="password"  placeholder="Enter password" /></td>
-            <td><?php  echo $passwordErr; ?></td>
+            <td class="error"><?php  echo $passwordErr; ?></td>
         </tr>
         <tr>
-            <td>Upload File</td>
-            <td><input type="file" name="fileupload" id="fileupload"/> </td>
+            <td>User Type</td>
+             <td>
+    <select name="usertype" id="usertype">
+        <option value="" <?php if($usertype== '') echo 'selected'; ?>></option>
+        <option value="customer" <?php if($usertype== 'customer') echo 'selected'; ?>>Customer</option>
+        <option value="deliverystaff" <?php if($usertype == 'deliverystaff') echo 'selected'; ?>>Delivery Staff</option>
+        <option value="admin" <?php if($usertype == 'admin') echo 'selected'; ?>>Admin</option>
+    </select>
+</td>
+<td class="error"><?php echo $usertypeErr; ?></td>
+
+
         </tr>
 
         <tr>
             <td></td>
-            <td><?php echo $loginErr; ?></td>
+            <td class="error"><?php echo $signUpErr; ?></td>
         </tr>
         <tr>
             <td><input type="submit" name="signup" value="Sign Up"/> </td>
         </tr>
     </table>
 </form>
+</div>
 </body>
 </html>
