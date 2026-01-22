3379af5c0d1001b4ed2e7487ed355f78dc4c9711
Tanjulala <168168212+Tanjulala@users.noreply.github.com>
2026-01-22 09:55:37 +0600

    updated login

--- customer/Controller/confirmOrder.php
+++ customer/Controller/confirmOrder.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include "../Model/databaseConnection.php";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db = new DatabaseConnection();
     $conn = $db->openConnection();
